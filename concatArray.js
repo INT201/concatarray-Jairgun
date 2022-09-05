a35a2ed795ada13135ea45ae85a9cb790e1e8d52
Jedsada <jasda.chot@kmutt.ac.th>
2022-09-05 14:28:09 +0700

    test

--- concatArray.js
+++ concatArray.js
@@ -2,8 +2,11 @@ const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   //code here
-  if (array1 == null? undefined:array2 == null? undefined:undefined);
-  else return concatArray.slice(0,concatArray.length);
+  if ((concatArray == null) || (concatArray == undefined)){
+    return undefined
+  }
+  // if (array1 == null? undefined:array2 == null? undefined:undefined);
+  // else return concatArray.slice(0,concatArray.length);
   // else if (concatArray.length == 0){return concatArray(array1,array2)}
   // else return concatArray.slice(0,concatArray.length)
 
