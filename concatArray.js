092ef356894823e4af2796bb167372a6c7d3eda8
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-09-10 15:27:48 +0700

    test

--- concatArray.js
+++ concatArray.js
@@ -2,6 +2,9 @@ const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   //code here
+  if(array1.includes(null || undefined) && array2.includes(null || undefined)){
+    return undefined;
+  }
   return array1.concat(array2);
 }
 module.exports = concatArray
